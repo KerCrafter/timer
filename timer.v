12d47b206bb83b6b9c0ece73360735d8db4b9c4f
KerCrafter <clemkerneur@gmail.com>
2025-11-02 14:13:12 +0100

    reset management

--- timer.v
+++ timer.v
@@ -3,6 +3,7 @@ module timer #(
     parameter FIRST_TICK_AFTER_DELAY = 0
 )(
     input  wire clk,
+    input  wire reset,
     input  wire enable,
     output wire tick
 );
@@ -10,6 +11,9 @@ module timer #(
     reg [$clog2(CLK_COUNT)-1:0] cnt = 0;
 
     always @(posedge clk) begin
+      if (reset) begin
+        cnt <= 0;
+      end else begin
         if (enable) begin
             if (cnt == 0)
                 cnt <= 1;
@@ -20,9 +24,11 @@ module timer #(
         end else begin
             cnt <= 0;
         end
+      end
     end
 
-    assign tick = (FIRST_TICK_AFTER_DELAY == 0 && enable && cnt == 1) ? 1'b1 :
+    assign tick = (reset) ? 1'b1 :
+                  (FIRST_TICK_AFTER_DELAY == 0 && enable && cnt == 1) ? 1'b1 :
                   (FIRST_TICK_AFTER_DELAY == 1 && enable && cnt == CLK_COUNT - 1) ? 1'b1 : 1'b0;
 
 endmodule
